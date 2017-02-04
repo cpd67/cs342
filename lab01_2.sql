c27c7ad6007b3ab8f9d14da4a91d3a316c53c4ca
Chris Dilley <cpd5@students.calvin.edu>
2017-02-03 19:27:16 -0500

    Finish Lab01

---
+++ lab01_2.sql
@@ -0,0 +1,10 @@
+-- Look at the schema 
+DESCRIBE dual;
+
+-- Get the data values from dual
+SELECT * FROM dual;
+
+-- Examples:
+-- + You can use it as a practice table for someone who has had no experience with SQL. 
+-- 	  They could use it to practice the basic commands, like SELECT and DESCRIBE.
+-- + You can use it as an example of how a table schema should look like.
