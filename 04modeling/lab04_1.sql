0dbcee120ad462c59f68b7714c10c190ee87bfa2
Chris Dilley <cpd5@students.calvin.edu>
2017-02-24 12:03:11 -0500

    Start Lab 04

---
+++ 04modeling/lab04_1.sql
@@ -0,0 +1,38 @@
+-- This command file loads an experimental person relation.
+-- The data conforms to the following assumptions:
+--     * Person IDs and team names are unique for people and teams respectively.
+--     * People can have at most one mentor.
+--     * People can be on many teams, but only have one role per team.
+--     * Teams meet at only one time.
+--
+-- CS 342
+-- Spring, 2017
+-- kvlinden
+
+drop table AltPerson;
+
+-- Exercise 4.1.a:
+
+-- Informally, you have many attributes that should belong to different entities located inside
+-- of the same relation. The team attributes should belong to a separate team relation, not to the 
+-- AltPerson relation. 
+--                                                   
+CREATE TABLE AltPerson (
+	personId integer,
+	name varchar(10),
+	status char(1),
+	mentorId integer,
+	mentorName varchar(10),
+	mentorStatus char(1),
+    teamName varchar(10),
+    teamRole varchar(10),
+    teamTime varchar(10)
+	);
+
+INSERT INTO AltPerson VALUES (0, 'Ramez', 'v', 1, 'Shamkant', 'm', 'elders', 'trainee', 'Monday');
+INSERT INTO AltPerson VALUES (1, 'Shamkant', 'm', NULL, NULL, NULL, 'elders', 'chair', 'Monday'); 
+INSERT INTO AltPerson VALUES (1, 'Shamkant', 'm', NULL, NULL, NULL, 'executive', 'protem', 'Wednesday');
+INSERT INTO AltPerson VALUES (2, 'Jennifer', 'v', 3, 'Jeff', 'm', 'deacons', 'treasurer', 'Tuesday');
+INSERT INTO AltPerson VALUES (3, 'Jeff', 'm', NULL, NULL, NULL, 'deacons', 'chair', 'Tuesday');
+
+
