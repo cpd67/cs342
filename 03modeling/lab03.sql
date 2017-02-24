1a2cb051850ef248db3601ee875c3e0c598fd1dd
Chris <codemonkey941@gmail.com>
2017-02-24 18:19:04 -0500

    Finish Homework 3. Still need to test the two sql files.

--- 03modeling/lab03.sql
+++ 03modeling/lab03.sql
@@ -12,6 +12,7 @@ drop table PersonTeam;
 drop table Team;
 drop table Person;
 drop table Homegroup;
+drop table Request;
 drop table HouseHold;
 
 -- Household table
@@ -24,6 +25,18 @@ create table HouseHold(
 	phoneNumber char(12)
 	);
 
+-- Exercise 3.3
+create table Request (
+	dateSubmitted date PRIMARY KEY, 
+	text varchar(150),
+	accessCode char(1),
+	response varchar(150)
+	householdNumber integer,
+	-- In the event a HouseHold record is deleted, 
+	-- delete all requests from that HouseHold.
+	FOREIGN KEY(householdNumber) REFERENCES HouseHold(ID) ON DELETE CASCADE
+	);
+	
 -- Homegroup table
 create table Homegroup (
 	ID integer PRIMARY KEY,
@@ -44,6 +57,7 @@ create table Person (
 	homegroupID integer,
 	role varchar(30),	
 	membershipStatus char(1) CHECK (membershipStatus IN ('m', 'a', 'c')),
+	requestID date,
 	-- Have a recursive reference to the ID of a Person record for the mentorID.
 	FOREIGN KEY(mentorID) REFERENCES Person(ID) ON DELETE SET NULL,
 	-- Due to the 1-many relationship between the Person and HouseHold tables, 
@@ -53,6 +67,8 @@ create table Person (
 	-- Since a Person record might not belong to a Homegroup, set this to NULL 
 	-- if the Homegroup record that they belong to is deleted.
 	FOREIGN KEY(homegroupID) REFERENCES Homegroup(ID) ON DELETE SET NULL, 
+	-- Request IDs can be NULL, as a Request can be assigned to 0 or 1 Persons.
+	FOREIGN KEY(requestID) REFERENCES Request(dateSubmitted) ON DELETE SET NULL,
 	-- Check the foreign keys, making sure that they are not be negative.
 	-- (The mentorID can be NULL, as a Person may not be a mentor to anyone or have a mentor themselves). 
 	-- (The homegroupID can be NULL, as a Person may not belong to a Homegroup).
@@ -86,20 +102,24 @@ create table PersonTeam (
 	FOREIGN KEY(teamID) REFERENCES Team(ID) ON DELETE CASCADE
 	);
 
-
 INSERT INTO Household VALUES (0,'2347 Oxford Dr. SE','Grand Rapids','MI','49506','616-243-5680');
+-- New record
+INSERT INTO Household VALUES (1,'1234 Candy Cane Dr.','Gumdrop City','Candyland','11111','123-456-0000');
 
--- New records
 INSERT INTO Homegroup VALUES (0, 'Bible Study #1', 'The Gospels');
 INSERT INTO Homegroup VALUES (1, 'Bible Study #2', 'Minor Prophets');
 
-INSERT INTO Person VALUES (0,'mr.','Keith','VanderLinden',NULL,0,0,'husband','m');
-INSERT INTO Person VALUES (1,'ms.','Brenda','VanderLinden',0,0,0,'wife','m');
--- New records
-INSERT INTO Person VALUES (2,'mr.','James','Smith',0,0,1,'gardner', 'a');
-INSERT INTO Person VALUES (3,'mrs.','Kendra','Jones',1,0,0,'maid','a');
+-- New records	
+INSERT INTO Request VALUES(DATE '2017-01-01', 'This house needs prayers.', 'a', 'I will pray for you.', 0);
+INSERT INTO Request VALUES(DATE '2017-02-01', 'This house also needs prayers.', 'b', 'I will pray for you.', 1);
 
--- New records
+INSERT INTO Person VALUES (0,'mr.','Keith','VanderLinden',NULL,0,0,'husband','m', NULL);
+INSERT INTO Person VALUES (1,'ms.','Brenda','VanderLinden',0,0,0,'wife','m', DATE '2017-02-01');
+INSERT INTO Person VALUES (2,'mr.','James','Smith',0,0,1,'gardner', 'a', NULL);
+INSERT INTO Person VALUES (3,'mrs.','Kendra','Jones',1,0,0,'maid','a', NULL);
+-- New record
+INSERT INTO Person VALUES (4,'mr.','Gingerbread','Man',NULL,1,0,'candyman','m',DATE '2017-01-01');
+
 INSERT INTO Team VALUES (0, 'Music Team', 'To make music!');
 
 INSERT INTO PersonTeam VALUES (0, 0, 'Leader', DATE '2017-01-21', DATE '2017-01-29');
